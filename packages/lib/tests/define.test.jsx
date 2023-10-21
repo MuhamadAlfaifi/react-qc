d4dc829ac7681cc7676b4ab3a08e8c72b7822954
Muhamad Alfaifi <muhamad.alfaifi@gmail.com>
2023-10-21 04:34:46 +0300

    update tests

--- packages/lib/tests/define.test.jsx
+++ packages/lib/tests/define.test.jsx
@@ -18,29 +18,48 @@ it('does not fail when creating query component', () => {
 
 it('passes query component props to queryFn', async () => {
   const keyFn = (variables) => [variables];
-  const queryFn = ({ queryKey }) => Promise.resolve(queryKey);
+  const queryFn = ({ queryKey }) => Promise.resolve([queryKey]);
 
   const MyQueryComponent = defineQueryComponent({ queryFn }, keyFn);
+  const MyInfiniteQueryComponent = defineInfiniteQueryComponent({ 
+    queryFn,
+    getNextPageParam: (_, pages) => pages.length + 1,
+  }, keyFn);
 
   expect(MyQueryComponent).toBeDefined();
+  expect(MyInfiniteQueryComponent).toBeDefined();
 
-  function MyHook() {
+  function MyQueryHook() {
     const { data } = MyQueryComponent.useQuery({ myProp: 'my-key-lajksdf', value1: 'value1-jioajsdf', value2: 'value2-joifds' });
-    return <div data-testid="my-hook">{JSON.stringify(data)}</div>;
+    return <div data-testid="my-query-hook">{JSON.stringify(data)}</div>;
+  }
+
+  function MyInfiniteHook() {
+    const { data } = MyInfiniteQueryComponent.useInfiniteQuery({ myProp: 'my-key-lajksdf', value1: 'value1-jioajsdf', value2: 'value2-joifds' });
+    return <div data-testid="my-infinite-query-hook">{JSON.stringify(data)}</div>;
   }
 
   const { getByTestId } = render(
     <>
+      {/* query */}
       <MyQueryComponent variables={{ myProp: 'my-key', value1: 'value1', value2: 'value2' }} render={({ data }) => 
         <div data-testid="my-query-component">{JSON.stringify(data)}</div>
       } />
-      <MyHook />
+      <MyQueryHook />
+
+      {/* infinite query */}
+      {/* <MyInfiniteQueryComponent variables={{ myProp: 'my-key', value1: 'value1', value2: 'value2' }} render={({ data }) => 
+        <div data-testid="my-infinite-query-component">{JSON.stringify(data)}</div>
+      } />
+      <MyInfiniteHook /> */}
     </>
   );
 
   await waitFor(() => {
     expect(getByTestId('my-query-component')).toHaveTextContent('[{"myProp":"my-key","value1":"value1","value2":"value2"}]')
-    expect(getByTestId('my-hook')).toHaveTextContent('[{"myProp":"my-key-lajksdf","value1":"value1-jioajsdf","value2":"value2-joifds"}]')
+    expect(getByTestId('my-query-hook')).toHaveTextContent('[{"myProp":"my-key-lajksdf","value1":"value1-jioajsdf","value2":"value2-joifds"}]')
+    // expect(getByTestId('my-infinite-query-component')).toHaveTextContent('[{"myProp":"my-key","value1":"value1","value2":"value2"}]')
+    // expect(getByTestId('my-infinite-query-hook')).toHaveTextContent('[{"myProp":"my-key-lajksdf","value1":"value1-jioajsdf","value2":"value2-joifds"}]')
   })
 })
 
