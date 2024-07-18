4bff96d95be7fbff5928df4d151e1b51428b4cd1
Muhamad Alfaifi <muhamad.alfaifi@gmail.com>
2024-07-18 23:00:02 +0300

    add lerna

--- scripts/cp-readme.sh
+++
@@ -1,3 +0,0 @@
-cp ./README.md ./packages/iii/README.md
-cp ./README.md ./packages/iv/README.md
-cp ./README.md ./packages/v/README.md
--- scripts/pnpm.sh
+++
@@ -1,8 +0,0 @@
-cd packages/v
-pnpm $1 $2
-
-cd ../iv
-pnpm $1 $2
-
-cd ../iii
-pnpm $1 $2
